5d25cd0301ec90939654e03c53f47bba3504655d
nipunkhattri321 <nipunkhattri321@gmail.com>
2023-05-09 23:37:06 +0530

    commit

--- database.js
+++ database.js
@@ -2,13 +2,15 @@ import mysql from "mysql";
 import dotenv from "dotenv";
 dotenv.config();
 
-
 const conn = mysql.createConnection({
     host:process.env.Host,
+    port:process.env.port,
     user:process.env.Username,
     password:process.env.Password,
-    database:process.env.database
+    database:process.env.database,
 })
+
+console.log(conn);
 
 conn.connect(function(err){
     if(err) throw err;
